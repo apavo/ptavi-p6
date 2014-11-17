6965a7e36314839b6329a0272b7c917e7acfea0a
alberto pavo blas <apavo@iota33.aulas.gsyc.es>
2014-11-17 10:57:42 +0100

    server y client

--- client.py
+++ client.py
@@ -10,26 +10,31 @@ import sys
 try:
     # Dirección IP del servidor.
     SERVER = 'localhost'
-    METODO = sys.argv[1]
+    METODO = sys.argv[1].upper()
     cliente = sys.argv[2].split("@")
     LOGIN = cliente[0]
     cliente = cliente[1].split(":")
     IP = cliente[0]
     PORT = int(cliente[-1])
-
+    
+    if METODO == "INVITE":
+        line = 'INVITE ' + 'sip:' + LOGIN + '@' + IP + 'SIP/2.0' + '\r\n'  + '\r\n'
+    elif METODO == "BYE":
+        line = 'BYE' + 'sip:' + LOGIN + '@' +  IP + 'SIP/2.0' + '\r\n'  + '\r\n'
+    else:
+        line = METODO + ' sip:' + LOGIN + '@' +  IP + 'SIP/2.0' + '\r\n'  + '\r\n'
 
     # Contenido que vamos a enviar
-    LINE = '¡Hola mundo!'
+    
 
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
-
-    print "Enviando: " + LINE
-    my_socket.send(LINE + '\r\n')
+    
+    print "Enviando: " + line
+    my_socket.send(line + '\r\n')
     data = my_socket.recv(1024)
-
     print 'Recibido -- ', data
     print "Terminando socket..."
 
@@ -43,6 +48,8 @@ except ValueError:
 except IndexError:
     print "Usage: python client.py method receiver@IP:SIPport "
 
+except NameError:
+      print "Usage: python client.py method receiver@IP:SIPport "
+
 except socket.error:
-    print "Error: No server listening at" + " " + IP + " " + str(PORT)
-    
+    print "Error: No server listening at" + " " + IP + " " + str(PORT)    
