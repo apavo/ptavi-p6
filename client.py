efae09da020ce18737982a08de94449d85d81e4e
Alberto <alberto@ubuntu.(none)>
2014-11-23 20:03:15 +0100

    p6

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ import sys
 def procesar_contestacion(data):
     contestacion=data.split(" ")
     if contestacion[-1] == "OK\r\n\r\n":
-        line = "ACK " + 'sip:' + LOGIN + '@' + IP + 'SIP/2.0' + '\r\n'  + '\r\n'
+        line = "ACK " + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0' + '\r\n'  + '\r\n'
         my_socket.send(line)
 try:
     # Dirección IP del servidor.
@@ -22,7 +22,7 @@ try:
     IP = cliente[0]
     PORT = int(cliente[-1])
      # Contenido que vamos a enviar
-    line = METODO + ' ' + 'sip:' + LOGIN + '@' + IP + 'SIP/2.0' + '\r\n'  + '\r\n'
+    line = METODO + ' ' + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0' + '\r\n'  + '\r\n'
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -36,20 +36,23 @@ try:
     print data
     contestacion=data.split(' ')
     procesar_contestacion(data)
+    data = my_socket.recv(1024)
+    line =  'BYE ' + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0' + '\r\n'  + '\r\n'
+    my_socket.send(line)
     print "Terminando socket..."
     
     # Cerramos todo
     my_socket.close()
     print "Fin."
 
-#except ValueError: 
- #  print "Usage: python client.py method receiver@IP:SIPport "
+except ValueError: 
+    print "Usage: python client.py method receiver@IP:SIPport "
 
-#except IndexError:
- #   print "Usage: python client.py method receiver@IP:SIPport "
+except IndexError:
+    print "Usage: python client.py method receiver@IP:SIPport "
 
-#except NameError:
- #     print "Usage: python client.py method receiver@IP:SIPport "
+except NameError:
+    print "Usage: python client.py method receiver@IP:SIPport "
 
 except socket.error:
     print "Error: No server listening at" + " " + IP + " " + str(PORT)    
