c9a2fabf6de13685d1dbe8310c3af094db3e4eff
alberto pavo blas <apavo@iota22.aulas.gsyc.es>
2014-11-24 10:59:51 +0100

    p6 con pep8

--- client.py
+++ client.py
@@ -7,10 +7,13 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 # Cliente SIP simple.
+
+
 def procesar_contestacion(data):
-    contestacion=data.split(" ")
+    contestacion = data.split(" ")
     if contestacion[-1] == "OK\r\n\r\n":
-        line = "ACK " + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0' + '\r\n'  + '\r\n'
+        line = "ACK " + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0'
+        line += '\r\n' + '\r\n'
         my_socket.send(line)
 try:
     # Dirección IP del servidor.
@@ -22,30 +25,30 @@ try:
     IP = cliente[0]
     PORT = int(cliente[-1])
      # Contenido que vamos a enviar
-    line = METODO + ' ' + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0' + '\r\n'  + '\r\n'
+    line = METODO + ' ' + 'sip:' + LOGIN + '@' + IP
+    line += ' SIP/2.0' + '\r\n' + '\r\n'
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
-    
     print "Enviando: " + line
     my_socket.send(line + '\r\n')
-   
-   
     data = my_socket.recv(1024)
     print data
-    contestacion=data.split(' ')
+    contestacion = data.split(' ')
     procesar_contestacion(data)
     data = my_socket.recv(1024)
-    line =  'BYE ' + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0' + '\r\n'  + '\r\n'
+    line = 'BYE ' + 'sip:' + LOGIN + '@' + IP + ' SIP/2.0' + '\r\n' + '\r\n'
     my_socket.send(line)
+    data = my_socket.recv(1024)
+    print data
     print "Terminando socket..."
-    
+
     # Cerramos todo
     my_socket.close()
     print "Fin."
 
-except ValueError: 
+except ValueError:
     print "Usage: python client.py method receiver@IP:SIPport "
 
 except IndexError:
@@ -55,4 +58,4 @@ except NameError:
     print "Usage: python client.py method receiver@IP:SIPport "
 
 except socket.error:
-    print "Error: No server listening at" + " " + IP + " " + str(PORT)    
+    print "Error: No server listening at" + " " + IP + " " + str(PORT)
--- server.py
+++ server.py
@@ -7,24 +7,39 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import SocketServer
 import sys
 import os
+
+
 class SIPHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server class
     """
-    def procesar(self, line,HOST,AUDIO):
+    def procesar(self, line, HOST, AUDIO):
+        """
+        Procesa los mensajes recibidos
+        """
+        # Guarda el metodo que nos envia el cliente
         linea = line.split(" ")
+        login = linea[1].split(":")
+        login = login[1].split("@")[0]
         metodo = linea[0]
-        if metodo == "INVITE" :
+        if metodo == "INVITE":
             line = 'SIP/2.0 100 Trying' + '\r\n' + '\r\n'
-            line += 'SIP/2.0 180 Ring' + '\r\n' + '\r\n'
-            line+= 'SIP/2.0 200 OK' + '\r\n' + '\r\n'
+            line += 'SIP/2.0 180 Ringing' + '\r\n' + '\r\n'
+            line += 'SIP/2.0 200 OK' + '\r\n' + '\r\n'
             self.wfile.write(line)
-        elif metodo == "ACK" :
-            aEjecutar = './mp32rtp -i ' + HOST + ' -p 23032 < ' + AUDIO
+        elif metodo == "ACK":
+            aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + AUDIO
             os.system(aEjecutar)
+        elif metodo == "BYE":
+            line = 'SIP/2.0 200 OK' + '\r\n' + '\r\n'
+            self.wfile.write(line)
         elif metodo != "INVITE" or "ACK" or "BYE":
             line = "SIP/2.0 405 Method Not Allowed" + '\r\n' + '\r\n'
             self.wfile.write(line)
+        else:
+            line = "SIP/2.0 400 Bad Request" + '\r\n' + '\r\n'
+            self.wfile.write(line)
+
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         while 1:
@@ -32,18 +47,18 @@ class SIPHandler(SocketServer.DatagramRequestHandler):
                 line = self.rfile.read()
                 if not line:
                     break
-                
+
                 print "El cliente nos manda " + line
-                self.procesar(line,HOST,AUDIO)
+                self.procesar(line, HOST, AUDIO)
                 # Si no hay más líneas salimos del bucle infinito
-                
-            
+
+
 if __name__ == "__main__":
     try:
         # Creamos servidor de eco y escuchamos
         HOST = sys.argv[1]
-        PORT= int(sys.argv[2])
-        AUDIO= sys.argv[3]
+        PORT = int(sys.argv[2])
+        AUDIO = sys.argv[3]
         serv = SocketServer.UDPServer((HOST, PORT), SIPHandler)
         print "Listening..."
         serv.serve_forever()
